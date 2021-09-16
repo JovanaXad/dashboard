cf2a7ece4f1f81681f425c51ed655552ebb17a8b
unknown <proenglishxad@gmail.com>
2021-09-16 23:07:55 +0200

    update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,6 @@ import Document from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
-  static displayName: any;
   static async getInitialProps(ctx: any) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
@@ -29,5 +28,3 @@ export default class MyDocument extends Document {
     }
   }
 }
-
-MyDocument.displayName = "MyDocument";
