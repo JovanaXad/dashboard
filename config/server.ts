c0e0ce096d85dceabb71b4939d5371c8c4567ab6
unknown <proenglishxad@gmail.com>
2021-09-17 20:39:29 +0200

    update

--- config/server.ts
+++ config/server.ts
@@ -1,4 +1,4 @@
 const dev = process.env.NODE_ENV !== "production";
 export const server = dev
   ? "http://localhost:3000"
-  : "https://dashboard-jovanaxad.vercel.app/";
+  : "https://dashboard-jovanaxad.vercel.app";
